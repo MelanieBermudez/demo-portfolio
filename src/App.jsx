aa5314e747f19c5ebb4b0dcbf2d45402513e8f8e
Melanie Bermudez <37756008+MelanieBermudez@users.noreply.github.com>
2024-10-09 18:08:09 -0400

    added icon libraries and adjusted css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import { Button, Card, CardBody, CardHeader, Container, Nav, NavItem, NavLink } from 'react-bootstrap';
+import { Link } from 'react-router-dom';  // React Router Link for navigation
+import { FaLinkedin} from 'react-icons/fa';
+
+
+
 import data from "../data.json"
 function App() {
 
@@ -45,27 +50,28 @@ function App() {
     <>
       <header className='header-box'>
         <div className='d-flex justify-content-between w-100 align-items-center'>
-          <Button onClick={toggleDarkMode} variant={isDarkMode ? "outline-light" : "outline-dark"} >
+          <Button onClick={toggleDarkMode} variant={isDarkMode ? "outline-light" : "outline-dark"} size="sm">
             {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
           </Button>
           <Nav className='justify-content-end'
-          activeKey="/home">
-          <NavItem>
-            <NavLink href='#experience'>Resume</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href='#contact'>Contact</NavLink>
-          </NavItem>
-        </Nav>
+            activeKey="/home">
+            <NavItem>
+              <NavLink href='#experience'>Resume</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='#contact'>Contact</NavLink>
+            </NavItem>
+          </Nav>
         </div>
       </header>
       <Container className='pt-2 '>
+        {/* <img src='/profile.jpg' alt='profile image' className='logo-img'/> */}
         <h1>Melanie Bermudez</h1>
         <p>Front-End Developer</p>
       </Container>
 
       <Container className='d-flex pb-3 justify-content-evenly'>
-        <blockquote><q>Creativity and Innovation will preserve</q></blockquote>
+        <blockquote><q><i>Crafting possibilities into reality</i></q></blockquote>
       </Container>
 
       <Container className='pt-4 info-box' id='experience'>
@@ -73,7 +79,7 @@ function App() {
           Experience
         </h2>
         <Card className='border-box m-2 text-start' >
-          <CardHeader >Accenture</CardHeader>
+          <CardHeader>Accenture</CardHeader>
           <CardBody>
             <ul >
               <li>
@@ -81,14 +87,13 @@ function App() {
               </li>
               <li>
                 Collaborated in Agile/Scrum teams, participating in daily stand-ups, sprint planning, and retrospectives to drive
-                project success.
+                project success
               </li>
               <li>
-                Collaborated in Agile/Scrum teams, participating in daily stand-ups, sprint planning, and retrospectives to drive
-                project success.
+                Turn static Photoshop designs into working browser-based web pages or emails
               </li>
               <li>Implemented the latest industry trends and best practices to optimize front-end architecture and improve
-                application scalability including test plans and decommission processes.</li>
+                application scalability including test plans and decommission processes</li>
               <li>React, JavaScript, HTML, CSS, Bootstrap 5, Node JS, Workfront, Abstract, Azure, Git, Copilot, Vite, Figma, Agile/Scrum methodologies, Storybook, BEM</li>
             </ul>
           </CardBody>
@@ -105,7 +110,7 @@ function App() {
               </li>
               <li>
                 Build end-to-end web-based component application, integrate with APIs in Java and create SQL model
-                while balancing speed, priority, and scope. Manual debugging and testing of scope.
+                while balancing speed, priority, and scope. Manual debugging and testing of scope
               </li>
               <li>React, JavaScript, HTML, CSS, Material UI, Java, Maven, SQL, Git, Jira, Confluence, Agile/Scrum methodologies, Storybook</li>
             </ul>
@@ -116,13 +121,13 @@ function App() {
           <CardBody>
             <ul >
               <li>
-                Gathered, reviewed and organized data from Computer Science school to reapply for the SINAES international accreditation program.
+                Gathered, reviewed and organized data from Computer Science school to reapply for the SINAES international accreditation program
               </li>
               <li>
                 Develop tools to track academic and extra-curricular activities carried out within the school. Set up a Moodle platform for online cultural courses offered by the University
               </li>
               <li>
-                Handle university&apos;s cultural program social media channels like Facebook and Instagram. Plan and create engaging content, respond to messages, launch and monitor performance of advertisements.
+                Handle university&apos;s cultural program social media channels like Facebook and Instagram. Plan and create engaging content, respond to messages, launch and monitor performance of advertisements
               </li>
               <li>Microsoft 365, SQL Server, Google Ads, Moodle</li>
             </ul>
@@ -199,8 +204,40 @@ function App() {
 
       <Container className='pt-4' id='contact'>
         <h5>Contact Information</h5>
-        <p>mebeca19@gmail.com</p>
-        <p>+1 973 494 6734</p>
+
+        <div>
+
+          <a
+            href="mailto:mebeca19@gmail.com"  // Replace with your email
+            className="contact-link"
+            aria-label="Send me an email"
+            style={{ color: isDarkMode ? '#fff' : '#000' }}
+          >
+            mebeca19@gmail.com
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="tel:+19734946734"  // Replace with your phone number, make sure to include the country code
+            className="contact-link"
+            aria-label="Call me"
+            style={{ color: isDarkMode ? '#fff' : '#000' }}
+          >
+            +1 973 494 6734
+          </a>
+        </div>
+        <div className="profile-link pt-1">
+
+          <a
+            href="https://www.linkedin.com/in/melaniebermudez"
+            target="_blank"
+            rel="noopener noreferrer"  // Security feature
+            className="linkedin-link"
+          >
+            <FaLinkedin size={30} style={{ color: '#0A66C2' }} />
+          </a>
+        </div>
       </Container>
 
       {/* <footer className='pt-4 '>
